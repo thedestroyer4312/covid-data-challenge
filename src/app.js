5b267d07482083c69241c1ffe68935468e730ce1
Eric <57415580+twojineric@users.noreply.github.com>
2020-09-10 17:20:41 -0700

    Improved Survey

--- src/app.js
+++ src/app.js
@@ -1,11 +1,27 @@
 const button = document.getElementById('submit');
 const output = document.querySelector(".result > p");
+const coll = document.getElementsByClassName("collapsible");
 
 main();
 
 function main() {
-    button.addEventListener('click', () => {
-        let riskPer = Math.random() * 100;
-        output.innerHTML = `Based on the data given, you are at a ${riskPer}% increased risk of COVID19.`;
-    });
+
+    for (let i = 0; i < coll.length; i++)
+    {
+        coll[i].addEventListener('click', togglePanel);
+        
+        button.addEventListener('click', () => {
+            let riskPer = Math.random() * 100;
+            output.innerHTML = `Based on the data given, you are at a ${riskPer.toFixed(2)}% increased risk of COVID19.`;
+        });
+    }
+}
+
+function togglePanel() {
+    let content = this.nextElementSibling;
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
 }
