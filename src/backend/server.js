ba26628a3bbf8b9b637348cba85ba95d5b3f9dc9
Eric <57415580+twojineric@users.noreply.github.com>
2020-09-18 17:31:34 -0700

    API now references data
    Files stored on Google Cloud Storage can now be referenced with a POST method.

--- src/backend/server.js
+++ src/backend/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const { Storage } = require('@google-cloud/storage');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,8 +11,47 @@ app.options('*', cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+const storage = new Storage({
+    keyFilename: process.env.PATHTOFILE
+});
+
+app.post('/testReadFile', function (request, response) {
+    const file = storage.bucket('activity-risk').file('activityRiskJson.json');
+    let buf = '';
+    file.createReadStream()
+        .on('data', (rawData) => {
+            //read the file from the stream
+            buf = buf + rawData;
+        })
+        .on('end', () => {
+            //read the finished JSON file and calculate
+            let activityRiskFile = JSON.parse(buf);
+            let activityArr = request.body.activities;
+            let riskLvl = 0;
+            for(let task of activityArr) {
+                riskLvl = riskLvl + activityRiskFile["activityRisk"][task];
+            }
+            response.header("Access-Control-Allow-Origin", "*");
+            if(activityArr.length == 0) {
+                response.send({
+                    avgRisk: 0
+                });
+                return;
+            }
+            else {
+                response.send({
+                    avgRisk: riskLvl/(activityArr.length),
+                });
+            }
+        })
+        .on('error', () => {
+            return console.log("ERROR");
+        });
+
+});
+
 app.get('/', function (request, response) {
-  response.send('Hello there!');
+    response.send('Hello there!');
 });
 
 app.get('/getRandomNumber', function (request, response, next) {
